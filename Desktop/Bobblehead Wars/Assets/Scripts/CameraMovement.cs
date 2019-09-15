303d38e2c2f2860808ccc6817cc21f2bd44a9e72
Ryan Massia <ryanmassia6@gmail.com>
2019-09-14 20:29:08 -0400

    Chapter 3

---
+++ Desktop/Bobblehead Wars/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject Follow_Target; // allows you to put a game object in here to follow
+    public float movespeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Follow_Target != null)
+        {
+            // has the camera follow behind the targeted object
+            transform.position = Vector3.Lerp(transform.position,
+            Follow_Target.transform.position, Time.deltaTime * movespeed);
+        }
+    }
+}
---
+++ Desktop/Bobblehead Wars/Assets/Scripts/Gun.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Gun : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+    public Transform launch_position;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+   
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) //if button is pressed will go into loop
+        {
+            if (!IsInvoking("fireBullet"))
+            {
+                InvokeRepeating("fireBullet", 0f, 0.1f); // valls the fire bullet function till button is no longeer pressed
+            }
+  
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            CancelInvoke("fireBullet"); // when released function will stop
+        }
+    }
+    void fireBullet()
+    {
+        GameObject bullet = Instantiate(BulletPrefab) as GameObject; 
+        bullet.transform.position = launch_position.position;
+        bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * 100; // bullets movement speed
+    }
+}
