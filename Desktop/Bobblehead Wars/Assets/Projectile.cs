e83ad9dc8bed12176cd6a50591eb8d2a9be7c191
Ryan Massia <ryanmassia6@gmail.com>
2019-09-15 11:11:49 -0400

    projectile now deleted when not seen or collides added layers to player and bullet

---
+++ Desktop/Bobblehead Wars/Assets/Projectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject); // destroys object if it isnt seen on screen
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject); // destroys object when it collides with another object
+    }
+}
