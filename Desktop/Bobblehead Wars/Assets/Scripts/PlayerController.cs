0d497688c5edea8742de3b71f254eab28c145ee8
Ryan Massia <ryanmassia6@gmail.com>
2019-09-15 10:54:47 -0400

    added comtroller to player so he cant phase into objects

--- Desktop/Bobblehead Wars/Assets/Scripts/PlayerController.cs
+++ Desktop/Bobblehead Wars/Assets/Scripts/PlayerController.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float movespeed = 50.0f; 
+    public float movespeed = 50.0f;
+    private CharacterController characterController;
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = transform.position;
+        // old code for the rididbody
+        /*  Vector3 pos = transform.position;
         pos.x += movespeed * Input.GetAxis("Horizontal") * Time.deltaTime; //allows the player to move along the x  and z axix with their set speed
         pos.z += movespeed * Input.GetAxis("Vertical") * Time.deltaTime;
-        transform.position = pos; 
+        transform.position = pos;
+       */
+
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), //stores movement direction of the player
+                                           0, Input.GetAxis("Vertical"));
+        characterController.SimpleMove(moveDirection * movespeed); //allows charcter to move but not through objects
     }
 }
