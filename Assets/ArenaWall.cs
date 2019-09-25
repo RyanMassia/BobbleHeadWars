1d567175e1fe71b1f24e917d91d7be9a45e7f15b
Ryan Massia <ryanmassia6@gmail.com>
2019-09-25 19:50:42 -0400

    chapter 6: Walls animate when collider occupied

--- Assets/ArenaWall.cs
+++ Assets/ArenaWall.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class ArenaWall : MonoBehaviour
 {
+    private Animator arenaAnimator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject arena = transform.parent.gameObject; // gets the parent child 
+        arenaAnimator = arena.GetComponent<Animator>(); // calls for a reference of the animator 
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        arenaAnimator.SetBool("islowered", true); // when hero enters collider is triggered sets islowered to true 
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        arenaAnimator.SetBool("islowered", false); // when hero leaves collider sets islowered to false
     }
 }
