575c0b55e89f038e39988500ca62e59535c845ca
Ryan Massia <ryanmassia6@gmail.com>
2019-09-28 16:10:54 -0400

    Chapter 7: Upgrade now spawns on the map

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,24 +7,52 @@ public class GameManager : MonoBehaviour
     public GameObject player; //represents the hero
     public GameObject[] spawnPoints; //represents all the spawn points
     public GameObject alien; //represents the alien menace
+    public GameObject upgradePrefab; // player must collide with to get the update
     public int maxAliensOnScreen; //how many enemies on screen
     public int totalAliens; //counts all enemies on screen
     public float minSpawnTime; // control the rate of the ememies spawning
     public float maxSpawnTIme;  
     public int aliensPerSpawn; // how many aliens will spawn per time
     private int aliensOnScreen = 0; //shows total aliens will tell manager if more are needed or not
+    public Gun gun; // reference to Gun script
+    public float upgradeMaxTimeSpawn = 7.5f;
+
+    private bool spawnedUpgrade = false;
+    private float actualUpgradeTime = 0;
+    private float currentUpgradeTime = 0;
     private float generatedSpawnTime = 0.0f; // trak time between spawns randomize to keep on toes
     private float currentSpawnTime = 0.0f; // track miliseconds since last spawn
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        actualUpgradeTime = Random.Range(upgradeMaxTimeSpawn - 3.0f, upgradeMaxTimeSpawn); // pgrade time is a random number generated
+        actualUpgradeTime = Mathf.Abs(actualUpgradeTime);
     }
 
     // Update is called once per frame
     void Update()
     {
+        currentUpgradeTime += Time.deltaTime; // s adds the amount of time from the past frame
+        if (currentUpgradeTime > actualUpgradeTime)
+        {
+            // 1s checks if the upgrade has already spawned
+            if (!spawnedUpgrade)
+            {
+                // 2 upgrade will appear in one of the alien spawn
+                int randomNumber = Random.Range(0, spawnPoints.Length - 1);
+                GameObject spawnLocation = spawnPoints[randomNumber];
+                // 3spawning the upgrade and associating the gun with it
+                GameObject upgrade = Instantiate(upgradePrefab) as GameObject;
+                UpGrade upgradeScript = upgrade.GetComponent<UpGrade>();
+                upgradeScript.gun = gun;
+                upgrade.transform.position = spawnLocation.transform.position;
+
+                // 4 upgrade has been spawned
+                spawnedUpgrade = true;
+                SoundManager.Instance.PlayOneShot(SoundManager.Instance.powerUpAppear);
+            }
+        }
         currentSpawnTime += Time.deltaTime; //will add all the time between each frame
         if (currentSpawnTime > generatedSpawnTime)
         {
---
+++ Assets/Scripts/UpGrade.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpGrade : MonoBehaviour
+{
+    public Gun gun;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        gun.UpgradeGun();
+        Destroy(gameObject);
+        SoundManager.Instance.PlayOneShot(SoundManager.Instance.powerUpPickup);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
